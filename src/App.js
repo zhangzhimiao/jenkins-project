e0281b58dd4bb66bd9c4f9b4eaaf9f1cea70d634
zhangzm@tita.com <zhangzm@tita.com>
2021-08-03 17:19:26 +0800

    测试重新构建

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         Empty Header
-      </header>
     </div>
   );
 }
